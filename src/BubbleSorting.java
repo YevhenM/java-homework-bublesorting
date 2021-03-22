c8f0abe35561a1d39ac2dcdbb14c45ebe749def4
YevhenM <marukhnyak.y@gmail.com>
2021-03-22 23:27:04 +0200

    fixed

--- src/BubbleSorting.java
+++ src/BubbleSorting.java
@@ -5,7 +5,7 @@ public class BubbleSorting {
     }
     private static void printSorted(int[] arr){
         for(int i=0; i < arr.length; i++){
-            for(int j=0; j < arr.length-1; j++){
+            for(int j=0; j < arr.length-i-1; j++){
                 if(arr[j]>arr[j+1]) {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
